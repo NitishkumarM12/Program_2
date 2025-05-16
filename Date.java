fe2ae036f87b75739508d8df3bcaf3c27624bdc0
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-05-16 12:13:31 +0530

    save file

--- Date.java
+++ Date.java
@@ -3,22 +3,25 @@ class student {
     private String name;
     private int rollNumber;
     private String course;
+    private String branch;
 
-    public student(String name, int rollNumber, String course) {
+    public student(String name, int rollNumber, String course, String branch) {
         this.name = name;
         this.rollNumber = rollNumber;
         this.course = course;
+        this.branch = branch;
     }
 
     public void display() {
         System.out.println("Student Name: " + name);
         System.out.println("Roll Number: " + rollNumber);
         System.out.println("Course: " + course);
+        System.out.println("Branch: " + branch);
     }
 }
 public class Date{
     public static void main(String[] args) {
-        student stud = new student("Alice", 101, "Computer Science");
+        student stud = new student("Alice", 101, "Computer Science", "AI");
         stud.display();
     }
 }
